3613597198404f24b9a556026bc9636b213b050c
Samuell1 <samuell.patro@gmail.com>
2019-02-25 12:10:32 +0100

    Change fields for locales

--- updates/add_new_fields_to_locales_table.php
+++ updates/add_new_fields_to_locales_table.php
@@ -10,14 +10,13 @@ class AddNewFieldsToLocalesTable extends Migration
         Schema::table('rainlab_translate_locales', function($table)
         {
             $table->string('domain')->nullable();
-            $table->boolean('is_domain_redirect')->default(0);
         });
     }
     public function down()
     {
         Schema::table('rainlab_translate_locales', function($table)
         {
-            $table->dropColumn(['domain', 'is_domain_redirect']);
+            $table->dropColumn(['domain']);
         });
     }
 }
